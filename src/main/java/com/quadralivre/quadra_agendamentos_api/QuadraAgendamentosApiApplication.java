b7966d2807f3a80b27d9c52a114fa2d99ba15776
wellingtondebrito <wellingtondebritosouza@gmail.com>
2025-05-19 13:19:53 -0300

    chore: inicia projeto spring com dependências atualizadas

---
+++ src/main/java/com/quadralivre/quadra_agendamentos_api/QuadraAgendamentosApiApplication.java
@@ -0,0 +1,13 @@
+package com.quadralivre.quadra_agendamentos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadraAgendamentosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadraAgendamentosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quadralivre/quadra_agendamentos_api/QuadraAgendamentosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quadralivre.quadra_agendamentos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadraAgendamentosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
